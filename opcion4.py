02068c043c4530c0b3a9b22bdb9a2267065f6fa2
luhussdev <lui.maraboli@duocuc.cl>
2025-06-13 20:54:45 -0400

    Add files via upload
    Se sube menú opción 4

---
+++ opcion4.py
@@ -0,0 +1,5 @@
+elif opcion == 4:
+    print("\nSaliendo del programa....")
+    print("Desarrollado por Claudio Alvarez, Felipe Lagos, Luis Maraboli")
+    print("Versión del programa 1.0")
+    break
