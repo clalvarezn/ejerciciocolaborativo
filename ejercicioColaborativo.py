7537072f40253391499bb865cb45bd37d8d5cd47
luhussdev <lui.maraboli@duocuc.cl>
2025-06-13 20:32:41 -0400

    Add files via upload
    Se incorporan excepciones al menú opción 2

--- ejercicioColaborativo.py
+++ ejercicioColaborativo.py
@@ -2,21 +2,30 @@
 
 
 elif opcion == "2":
-    busqueda_rut = input("Ingrese el rut a buscar: ")
-    encontrado = False
-    for af in datosclientes:
-        if af["rut"] == busqueda_rut:
-            print("Afiliado encontrado")
-            print("Nombre",af["nombre"])
-            print("Apellido", af["apellido_paterno"])
-            print("Edad", af["edad"])
-            print("Estado Civil", af["estado_civil"])
-            print("Genero", af["genero"])
-            print("Fecha de afiliación", af["fecha_afiliacion"])
-            encontrado = True
-            break
+    try:
+        busqueda_rut = input("Ingrese el RUT a buscar: ")
+        if busqueda_rut == "":
+            raise ValueError("El RUT no puede estar vacío.")
+
+        encontrado = False
+        for af in datosclientes:
+            if af["rut"] == busqueda_rut:
+                print("Afiliado encontrado:")
+                print("Nombre:", af["nombre"])
+                print("Apellido:", af["apellido"])
+                print("Edad:", af["edad"])
+                print("Estado civil:", af["estado_civil"])
+                print("Género:", af["genero"])
+                print("Fecha de afiliación:", af["fecha_afiliacion"])
+                encontrado = True
+                break
         if not encontrado:
-            print("Afiliado no encontrado")
+            print("Afiliado no encontrado.")
+    except ValueError as e:
+        print("Error:", e)
+    except Exception as e:
+        print("Ha ocurrido un error inesperado:", e)
+        
 
 
 
